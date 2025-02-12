c879771f59b76277652cbb2433db35be7d4f417a
rjk-info <therjkinfo@gmail.com>
2025-02-12 18:59:15 +0530

    Add files via upload

---
+++ Lecture4b/src/App.jsx
@@ -0,0 +1,14 @@
+// React web component (App.jsx)
+import './App.css';
+import MyLudo from './MyLudo.jsx';
+import TodoApp from './TodoApp.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
