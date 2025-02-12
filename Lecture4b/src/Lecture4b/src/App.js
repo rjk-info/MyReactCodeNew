16d72578e6488136b6ee1aa26178051ac22864e2
rjk-info <therjkinfo@gmail.com>
2025-02-12 18:56:21 +0530

    Add files via upload

---
+++ Lecture4b/src/Lecture4b/src/App.js
@@ -0,0 +1,14 @@
+// App.js (React Native Version)
+import React from 'react';
+import { View } from 'react-native';
+import TodoApp from './TodoApp';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <TodoApp />
+    </View>
+  );
+};
+
+export default App;
