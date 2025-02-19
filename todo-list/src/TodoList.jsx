aa2c75723500f13c2eda970260a6f27944d75654
iam-therjk <159906339+iam-therjk@users.noreply.github.com>
2025-02-19 16:58:33 +0530

    todoUpdate code in todo-list.jsx

--- todo-list/src/TodoList.jsx
+++ todo-list/src/TodoList.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
 export default function TodoList() {
+  let [todos, setTodos] = useState(["Sample task"]) ;
+  let [newTodo, setNewTodo] = useState("");
+
+  let addNewTask = () => {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  } ;
+
+  let updateTodoValue = (event) => {
+    setNewTodo(event.target.value) ;
+  };
+
   return(
   <div>
-    <h1>Todo List</h1>
-     </div>);
+    <input 
+    placeholder="add a task" 
+    onChange={updateTodoValue}
+    value={newTodo}
+    />
+    <br />
+    <button onClick={addNewTask}>Add Task</button>
+    <br /><br /><br /><br /><br /><br />
+    <hr />
+    <h4>Tasks Todo</h4>
+    <ul>
+    {todos.map((todo) => (
+        <li>{todo}</li>
+    ))}
+
+
+    </ul>
+     </div>
+     );
 }
